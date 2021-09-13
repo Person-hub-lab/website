737c294ab0f6ac78ed96f8ef14057a275e23038c
Person-hub-lab <79429471+Person-hub-lab@users.noreply.github.com>
2021-09-13 00:07:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function rickroll(){
+ console.log("Never gonna give you up");
+ console.log("Never gonna let you down");
+ console.log("Never gonna run around and desert you");
+ console.log("youtube WIP for everything");
+
+
+ }
+
+function website(){
+    console.log("This is a work in progress");
+    console.log("wikipedia");
+    console.log("Youtube");
+    console.log("this no exist");
+    console.log("bury me with my money");
+}
